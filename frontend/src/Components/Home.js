6cef2c65dede2f86d6b681c80e8ef3cfcda9c966
D.I.P Lakshan <onlineipl18@gmail.com>
2022-03-01 13:10:53 +0530

    created Home

---
+++ frontend/src/Components/Home.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react'
+import user from "../images/user.png";
+import Sidebar from "./Sidebar";
+import logout from "../images/logout.png";
+import lady from "../images/lady.png"
+
+
+
+
+function Home() {
+    return (
+        <div className='MainContainer'>
+                <div className='containermini'>
+                    
+                    <img src={user} className='user' />
+                    <Sidebar />
+                    <img src={logout} className='logout' />
+                </div>
+                <div className = "homebox1">
+                    <img src={lady} className='homeVector' />
+                    <span className="font-link">
+                        <h2 className='welcm-name'> Hello Software systems international pvt LTD!</h2>
+                     
+                    </span>
+                </div>   
+        </div>
+    )
+}
+
+export default Home
