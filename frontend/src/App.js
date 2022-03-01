e82244e5caca475fd65d62a4beed50fd50e0d790
D.I.P Lakshan <onlineipl18@gmail.com>
2022-03-01 13:10:32 +0530

    Change app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import "./App.css";
+import Home from "./Components/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ApplicationA from "./Components/ApplicationA";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/appA" component={ApplicationA} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
