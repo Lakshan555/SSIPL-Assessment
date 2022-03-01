cfda21207376316a41b357ac45ffc9b27b549242
D.I.P Lakshan <onlineipl18@gmail.com>
2022-03-01 16:25:36 +0530

    add items to side bar

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,30 +1,28 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 import user from "../images/user.png";
 import Sidebar from "./Sidebar";
 import logout from "../images/logout.png";
-import lady from "../images/lady.png"
-
-
-
+import lady from "../images/lady.png";
 
 function Home() {
-    return (
-        <div className='MainContainer'>
-                <div className='containermini'>
-                    
-                    <img src={user} className='user' />
-                    <Sidebar />
-                    <img src={logout} className='logout' />
-                </div>
-                <div className = "homebox1">
-                    <img src={lady} className='homeVector' />
-                    <span className="font-link">
-                        <h2 className='welcm-name'> Hello Software systems international pvt LTD!</h2>
-                     
-                    </span>
-                </div>   
-        </div>
-    )
+  return (
+    <div className="MainContainer">
+      <div className="containermini">
+        <img src={user} className="user" />
+        <Sidebar />
+        <img src={logout} className="logout" />
+      </div>
+      <div className="homebox1">
+        <img src={lady} className="homeVector" />
+        <span className="font-link">
+          <h2 className="welcm-name">
+            {" "}
+            Hello Software Systems International Pvt LTD!
+          </h2>
+        </span>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
--- frontend/src/Components/Sidebar.js
+++ frontend/src/Components/Sidebar.js
@@ -3,35 +3,38 @@ import "../App.css";
 import { NavLink } from "react-router-dom";
 
 function Sidebar() {
-	return (
-		<div className='Sidebar'>
-			<ul className='SidebarList'>
-				<NavLink
-					style={{ textDecoration: "none" }}
-					to='./'
-					activeClassName='active-nav'
-					className='row'>
-					Home
-				</NavLink>
+  return (
+    <div className="Sidebar">
+      <ul className="SidebarList">
+        <NavLink
+          style={{ textDecoration: "none" }}
+          to="./"
+          activeClassName="active-nav"
+          className="row"
+        >
+          Home
+        </NavLink>
 
-              
+        <NavLink
+          style={{ textDecoration: "none" }}
+          to="/appA"
+          activeClassName="active-nav"
+          className="row"
+        >
+          Application A
+        </NavLink>
 
-				<NavLink
-					style={{ textDecoration: "none" }}
-					to='/appA'
-					activeClassName='active-nav'
-					className='row'>
-					Application A
-				</NavLink>
-
-               
-
-              
-
-                
-			</ul>
-		</div>
-	);
+        <NavLink
+          style={{ textDecoration: "none" }}
+          to="/appB"
+          activeClassName="active-nav"
+          className="row"
+        >
+          Application B
+        </NavLink>
+      </ul>
+    </div>
+  );
 }
 
 export default Sidebar;
